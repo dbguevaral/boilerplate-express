8ed258866ef2c2e7b8ea4a8e46e5b1ae3e69bb21
Diego Guevara <brando.guevaral@gmail.com>
2025-08-04 15:50:55 -0500

    added a simple logger

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ let app = express();
 const absolutePath = __dirname + '/views/index.html';
 console.log("Hello World");
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
